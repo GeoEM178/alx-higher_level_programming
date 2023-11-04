d542b963fe75bd75e65a4740850f0c5088b18e3a
GeoEM178 <138987489+GeoEM178@users.noreply.github.com>
2023-11-04 17:43:23 +0200

    Update 10-divisible_by_2.py

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    mul = []
+    neww = []
     for i in range(len(my_list)):
         if my_list[i] % 2 == 0:
-            mul.append(True)
+            neww.append(True)
         else:
-            mul.append(False)
-    return (mul)
+            neww.append(False)
+    return (neww)
