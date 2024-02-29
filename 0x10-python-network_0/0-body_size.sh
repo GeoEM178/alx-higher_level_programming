9a8f505927dc077c70c35bdc5fb173ded6f902b2
GeoEM178 <geoem178@gmail.com>
2024-02-29 23:41:40 +0200

    Fix bugs

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # get data by bytes only
-if ( $# -gt 0) then
-    curl -s "$1" | wc -c
-fi
+curl -s "$1" | wc -c
--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # get the status code only
-if ( $# -gt 0 ) then
-    curl -sL "$1"
-fi
+curl -sL "$1"
